795077d1af542cbe09bec0d38f9a78dbd619a867
pkritt <krittapat37819@gmail.com>
2024-12-20 14:31:06 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
 
+int main(){
+    string grade;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    if(x==0) grade = "A";
+    else if(x==1) grade = "B+";
+    else if(x==2) grade = "B";
+    else if(x==3) grade = "C+";
+    else if(x==4) grade = "C";
+    else if(x==5) grade = "D+";
+    else if(x==6) grade = "D";
+    else if(x==7) grade = "F";
+    else grade = "W";
+    cout <<  "You will get " <<grade<<" in this 261102.";
+
+return 0;
+}
